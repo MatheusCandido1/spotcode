dc932026ac992815ba9144ade698a10e26975a36
matheus <matheus9bh@hotmail.com>
2020-04-16 13:55:19 -0300

    allow-no-auth-home

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,11 +2,16 @@ import React, {Fragment} from 'react';
 import 'react-bulma-components/dist/react-bulma-components.min.css'
 import { Button } from 'react-bulma-components'
 import 'app.scss';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom'
+
 
 const App = () => {
   return (
     <Fragment>
-      <Button class="button is-primary">Testando</Button>
+      <BrowserRouter>
+        <Routes/>
+      </BrowserRouter>
     </Fragment>
   )
 }
