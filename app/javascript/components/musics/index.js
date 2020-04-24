7a03170f76d51f610df72cea8d6180908fe77249
matheus <matheus9bh@hotmail.com>
2020-04-24 14:38:24 -0300

    add-music-template

---
+++ app/javascript/components/musics/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+​
+const Musics = (props) => {
+ return (
+   <Fragment>
+​
+   </Fragment>
+);
+}
+export default Musics;
---
+++ app/javascript/components/musics/music/index.js
@@ -0,0 +1,10 @@
+import React, { Fragment } from 'react';
+​
+const Music = (props) => {
+ return (
+   <Fragment>
+​
+   </Fragment>
+);
+}
+export default Music;
